7d4c7582f64e26f8e2afe27bc86d478cd7d031b8
leeseungeun <seungeun.data@gamil.com>
2021-05-29 22:45:28 +0900

    add upbit trade api class

--- api/upbit.py
+++ api/upbit.py
@@ -1,10 +1,13 @@
 import hashlib
 import os
 import uuid
+from enum import Enum
 from urllib.parse import urlencode
 
 import jwt
 import requests
+
+from api.interface import TradeAPIInterface
 
 
 class UpbitAPI:
@@ -38,3 +41,65 @@ class UpbitAPI:
         jwt_token = jwt.encode(payload, self.secret_key)
         authorize_token = 'Bearer {}'.format(jwt_token)
         return {"Authorization": authorize_token}
+
+
+class UpbitTradeAPI(UpbitAPI, TradeAPIInterface):
+    class Side(str, Enum):
+        BID = "bid"  # 매수
+        ASK = "ask"  # 매도
+
+    class OrderType(str, Enum):
+        LIMIT = "limit"     # 지정가 주문
+        PRICE = "price"     # 시장가 주문(매수)
+        MARKET = "market"   # 시장가 주문(매도)
+
+    def __init__(self):
+        super().__init__()
+
+        self.urls = {
+            "order": f"{self.BASE_URL}/v1/orders",
+            "cancel_order": f"{self.BASE_URL}/v1/order",
+        }
+
+    def _order(self, query):
+        headers = self.get_headers(query)
+        return self.session.post(self.urls["order"], params=query, headers=headers, timeout=3)
+
+    def market_sell(self, order_currency, pay_currency, units):
+        query = {
+            "market": f"{order_currency}-{pay_currency}",
+            "side": self.Side.ASK,
+            "volume": units,
+            "ord_type": self.OrderType.MARKET,
+        }
+        return self._order(query)
+
+    def market_buy(self, order_currency, pay_currency, units):
+        # TODO: price 계산
+        query = {
+            "market": f"{pay_currency}-{order_currency}",
+            "side": self.Side.BID,
+            "price": "100.0",
+            "ord_type": self.OrderType.PRICE,
+        }
+        return self._order(query)
+
+    def limit_sell(self, order_currency, pay_currency, units, price):
+        query = {
+            "market": f"{order_currency}-{pay_currency}",
+            "side": self.Side.ASK,
+            "volume": units,
+            "price": price,
+            "ord_type": self.OrderType.MARKET,
+        }
+        return self._order(query)
+
+    def limit_buy(self, order_currency, pay_currency, units, price):
+        query = {
+            "market": f"{order_currency}-{pay_currency}",
+            "side": self.Side.BID,
+            "volume": units,
+            "price": price,
+            "ord_type": self.OrderType.MARKET,
+        }
+        return self._order(query)
